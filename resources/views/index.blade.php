1681b54c807dd631a728174b99ea0690bea64db8
Worawiboon <sathone_w@silpakorn.edu>
2021-04-11 20:47:18 +0700

    upload to git succes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@if($message = Session::get('success'))
+    <p>{{ $message }}</p>
+@endif
+<a href="/new">create</a>
+<p> Your Quota Remaining {{ 10-count($urls) }}/10 </p>
+@if(!$urls->isEmpty())
+    <table>
+        <tr>
+            <td>Long URL</td>
+            <td>Short URL</td>
+            <td>Create</td>
+        </tr>
+        @foreach($urls as $url)
+            <tr>
+                <td>{{ $url->long_url }}</td>
+                <td><a href="/gt/{{ $url->short_url }}" target="_blank">{{ $url->short_url }}</a></td>
+                <td>{{ $url->created_at }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endif
